85dc4f88d886bbd9ec8b1a38ed4c3b113fb1574d
EasyMochi <mochi@okaeri.moe>
2025-07-04 13:27:53 +0100

    Refactored loot injection logic

--- src/main/java/com/fyre/cobblecuisine/loot/CobbleCuisineLootInjector.java
+++ src/main/java/com/fyre/cobblecuisine/loot/CobbleCuisineLootInjector.java
@@ -1,7 +1,6 @@
 package com.fyre.cobblecuisine.loot;
 
 import com.cobblemon.mod.common.CobblemonItems;
-
 import com.fyre.cobblecuisine.config.CobbleCuisineConfig;
 import com.fyre.cobblecuisine.item.food.BeanType;
 import com.fyre.cobblecuisine.item.CobbleCuisineItems;
@@ -11,9 +10,7 @@ import net.fabricmc.fabric.api.loot.v3.LootTableSource;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.LootTables;
+import net.minecraft.loot.*;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
@@ -23,7 +20,6 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.util.Identifier;
 
-import java.util.List;
 import java.util.Map;
 
 import static com.fyre.cobblecuisine.CobbleCuisine.LOGGER;
@@ -31,21 +27,19 @@ import static com.fyre.cobblecuisine.CobbleCuisine.LOGGER;
 public class CobbleCuisineLootInjector {
 	private static final Identifier GRASS_BLOCK_ID = Identifier.of("minecraft", "blocks/short_grass");
 
-	private static final List<RegistryKey<LootTable>> VILLAGE_CHESTS = List.of(
-			LootTables.VILLAGE_DESERT_HOUSE_CHEST,
-			LootTables.VILLAGE_PLAINS_CHEST,
-			LootTables.VILLAGE_SAVANNA_HOUSE_CHEST,
-			LootTables.VILLAGE_TAIGA_HOUSE_CHEST,
-			LootTables.VILLAGE_SNOWY_HOUSE_CHEST
+	private static final Map<RegistryKey<LootTable>, Item[]> BEAN_MAP = Map.of(
+			LootTables.VILLAGE_DESERT_HOUSE_CHEST, new Item[] { BeanType.ORANGE.item },
+			LootTables.VILLAGE_PLAINS_CHEST, new Item[] { BeanType.GREEN.item, BeanType.INDIGO.item },
+			LootTables.VILLAGE_SAVANNA_HOUSE_CHEST, new Item[] { BeanType.YELLOW.item },
+			LootTables.VILLAGE_TAIGA_HOUSE_CHEST, new Item[] { BeanType.RED.item },
+			LootTables.VILLAGE_SNOWY_HOUSE_CHEST, new Item[] { BeanType.BLUE.item, BeanType.VIOLET.item }
 	);
 
-	private static final Map<RegistryKey<LootTable>, Item> BEAN_MAP = Map.of(
-			LootTables.VILLAGE_DESERT_HOUSE_CHEST, BeanType.ORANGE.item,
-			LootTables.VILLAGE_PLAINS_CHEST, BeanType.GREEN.item,
-			LootTables.VILLAGE_SAVANNA_HOUSE_CHEST, BeanType.YELLOW.item,
-			LootTables.VILLAGE_TAIGA_HOUSE_CHEST, BeanType.RED.item,
-			LootTables.VILLAGE_SNOWY_HOUSE_CHEST, BeanType.BLUE.item
-	);
+	private static final Item[] COMMON_VILLAGE_ITEMS = new Item[] {
+			CobblemonItems.GALARICA_NUTS,
+			Items.TORCHFLOWER,
+			Items.TORCHFLOWER_SEEDS
+	};
 
 	private CobbleCuisineLootInjector() {}
 
@@ -56,28 +50,38 @@ public class CobbleCuisineLootInjector {
 
 	private static void onLootTableModify(RegistryKey<LootTable> key, LootTable.Builder table, LootTableSource source, WrapperLookup registries) {
 		if (key.getValue().equals(GRASS_BLOCK_ID)) {
-			addPool(table, CobbleCuisineConfig.data.dropRates.beanSeedsDropRate, CobbleCuisineItems.BEAN_SEEDS, 1, 1);
+			addDrop(table, CobbleCuisineItems.BEAN_SEEDS, CobbleCuisineConfig.data.dropRates.beanSeedsDropRate, 1, 1);
+			return;
 		}
 
-		if (VILLAGE_CHESTS.contains(key)) {
-			addPool(table, CobbleCuisineConfig.data.dropRates.galaricaNutDropRate, CobblemonItems.GALARICA_NUTS, 1, 3);
-			addPool(table, CobbleCuisineConfig.data.dropRates.torchflowerDropRate, Items.TORCHFLOWER, 1, 3);
-			addPool(table, CobbleCuisineConfig.data.dropRates.torchflowerSeedsDropRate, Items.TORCHFLOWER_SEEDS, 1, 1);
+		//noinspection ForLoopReplaceableByForEach
+		for (int i = 0; i < COMMON_VILLAGE_ITEMS.length; i++) {
+			Item item = COMMON_VILLAGE_ITEMS[i];
+			addDrop(table, item, getDropRate(item), 1, 3);
+		}
 
-			Item bean = BEAN_MAP.get(key);
-			if (bean != null) {
-				addPool(table, CobbleCuisineConfig.data.dropRates.beanDropRate, bean, 2, 5);
-			}
+		Item[] beans = BEAN_MAP.get(key);
+		if (beans == null) return;
+
+		//noinspection ForLoopReplaceableByForEach
+		for (int i = 0; i < beans.length; i++) {
+			addDrop(table, beans[i], CobbleCuisineConfig.data.dropRates.beanDropRate, 2, 5);
 		}
 	}
 
-	private static void addPool(LootTable.Builder table, float chance, Item item, float min, float max) {
+	private static void addDrop(LootTable.Builder table, Item item, float chance, float min, float max) {
 		table.pool(LootPool.builder()
-						.rolls(ConstantLootNumberProvider.create(1))
-						.conditionally(RandomChanceLootCondition.builder(chance))
-						.with(ItemEntry.builder(item))
-						.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(min, max)).build())
-						.build()
+				.rolls(ConstantLootNumberProvider.create(1))
+				.conditionally(RandomChanceLootCondition.builder(chance))
+				.with(ItemEntry.builder(item))
+				.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(min, max)))
 		);
+	}
+
+	private static float getDropRate(Item item) {
+		if (item == CobblemonItems.GALARICA_NUTS) return CobbleCuisineConfig.data.dropRates.galaricaNutDropRate;
+		if (item == Items.TORCHFLOWER) return CobbleCuisineConfig.data.dropRates.torchflowerDropRate;
+		if (item == Items.TORCHFLOWER_SEEDS) return CobbleCuisineConfig.data.dropRates.torchflowerSeedsDropRate;
+		return 0.1f;
 	}
 }
